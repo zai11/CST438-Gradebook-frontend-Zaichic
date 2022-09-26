95960b5f35b650e84ec475ca34dd999361a1f05e
Zaichic <zaipowell@rocketmail.com>
2022-09-26 13:49:25 -0700

    implement feature add student

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Gradebook from './components/Gradebook';
 import Assignment from './components/Assignment';
+import AddAssignment from './components/AddAssignment';
 import {BrowserRouter, Switch,  Route} from 'react-router-dom';
 
 function App() {
@@ -20,6 +22,7 @@ function App() {
        <Switch>
         <Route exact path='/' component={Assignment} />
         <Route path='/gradebook' component={Gradebook} />
+        <Route path='/assignment/add' component={AddAssignment} />
        </Switch>
       </BrowserRouter>
     </div>
