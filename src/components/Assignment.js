646815c0b753128a5f9dc2c918d417b89d5eab7f
Zaichic <zaipowell@rocketmail.com>
2022-11-01 19:49:45 -0700

    Fixed issues

--- src/components/Assignment.js
+++ src/components/Assignment.js
@@ -79,7 +79,7 @@ class Assignment extends React.Component {
               <div style={{ height: 450, width: '100%', align:"left"   }}>
                 <DataGrid rows={this.state.assignments} columns={columns} />
               </div>
-            <div class="button-controller">
+            <div className="button-controller">
               <Button component={Link} to={{pathname:'/gradebook',   assignment: assignmentSelected }} 
                     variant="outlined" color="primary" disabled={this.state.assignments.length===0}  style={{margin: 10}}>Grade</Button>
               <Button component={Link} to={{pathname: '/assignment/add', assignment: assignmentSelected}} variant="outlined" color="primary" style={{margin:10}}>Add Assignment</Button>
